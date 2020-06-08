ffec5257b454b98826ecfc2b60180096c6cefd54
AjaX_05 Desktop <majaysakthishankar@gmail.com>
2020-06-08 14:59:45 +0530

    Added some script using Ajay's PC via Teamviewer while letting him understand the usage of Git and how to push to GitHub when we commit.

---
+++ Testing_If_Elif_Else.py
@@ -0,0 +1,15 @@
+# Install Prettier Ext. for VS Code
+
+print("This is a Sample Code for testing IF, Elif and Else commands.")
+
+age = input("Hello, Please enter your Age here: ")
+
+if(age <= 13):
+    print("You come under the category of PG-13, meaning - you need to have parental guidance for certain actions to be taken on the Internet.")
+    exit()
+elif(age >= 18):
+    print("You're above 18, you're an adult now, be responsible and do not fall prey to your attractions unless they're worthy! :)")
+    exit()
+else:
+    print("You're in your Teen ages, becareful not to fall prey for any attractions you might come across ¯\_(ツ)_/¯")
+    exit()
